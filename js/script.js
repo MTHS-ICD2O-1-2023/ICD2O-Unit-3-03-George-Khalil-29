b4a148949ba88d2a444fef50b73ecd753e16678d
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-04-07 16:15:51 +0000

    save

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@
 // Created by: George , Khalil
 // Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateVolumeOfPyramid() {
+  // input
+  const length = parseFloat(document.getElementById('length-of-pyramid').value)
+  const width = parseFloat(document.getElementById('width-of-pyramid').value)
+  const height = parseFloat(document.getElementById('height-of-pyramid').value)
+  // process
+  const volumeOfPayrmid = (length * width * height) / 3
+  // output
+  document.getElementById('volume').innerHTML
+    = 'Volume is: ' + length * width * height / 3 + ' mm³.'
+}
+
